922690532fa6ec049311cba0ed68f8e9ee25e4a4
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2020-07-11 11:23:28 +0200

    __main__: closing jobs on exit

--- amit/__main__.py
+++ amit/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from .interactive import start_shell
-from .database import Base
+from .database import Base, Job
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
@@ -11,4 +11,7 @@ def main():
     engine = create_engine("sqlite:///amit.sqlite")
     session.configure(bind=engine)
     Base.metadata.create_all(engine)
-    start_shell(session)
+    try:
+        start_shell(session)
+    except KeyboardInterrupt:
+        session().update(Job).values(status="DONE")
