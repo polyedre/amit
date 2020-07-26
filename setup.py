85a357844aac7801742be5c82d96146aeb6450f4
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2020-07-26 18:53:12 +0200

    RELEASE: 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="amit",
-    version="0.5",
+    version="1.0",
     description="A pentest enumeration framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
